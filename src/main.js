f52018cfba539b8a7442f1a048b3ad9250f5b4eb
watain666 <watain666666@gmail.com>
2026-09-22 15:43:40 +0800

    chore(release): publish guide v1.8.31

--- src/main.js
+++ src/main.js
@@ -98,7 +98,7 @@ const SYNC_INTERVAL_MS   = 100;
 /* 지금 폰에 깔려 있는 화면이 몇 번째 판인지 알려 주는 표시.
    새로 올렸는데 화면이 그대로일 때, 옛 판이 남아 있는지 바로 확인할 수 있다.
    sw.js 의 CACHE_VERSION 과 같이 올려 주세요. */
-const BUILD = "v1.8.30";
+const BUILD = "v1.8.31";
 
 const REPO_URL = "https://github.com/watain666/Vaundy-Taiwan-2026";
 const FEEDBACK_URL = "https://www.threads.com/@brainginger/post/DdiLWztgen9";
